4aa2ce5092716deb616f3b4c5c6e3b5754eb6801
Devon D'Apuzzo <djdapz@aol.com>
2016-10-15 11:23:16 -0500

    Fix messed up merge conficts?

--- www/app/businesses/businessesDetails.controller.js
+++ www/app/businesses/businessesDetails.controller.js
@@ -5,54 +5,6 @@ var BusinessesDetailsController = function($scope, $http, NavBarService, $routeP
     $scope.business = undefined;
     $scope.error = undefined;
     $scope.waiting = true;
-
-    $http({
-        method: "GET",
-        url: " https://eecs394-clips-backend.herokuapp.com/business/" + $scope.businesssId
-    }).then(
-        function successCallback(response){
-            $scope.business = response.data[0];
-            $scope.waiting = false;
-        }, function errorCallback(error){
-            console.log(error);
-            $scope.error = error;
-            $scope.waiting = false;
-        }
-    );
-
-    $http({
-        method: "GET",
-        url: " https://eecs394-clips-backend.herokuapp.com/business/coupons/" + $scope.businesssId
-    }).then(
-        function successCallback(response){
-            $scope.coupons = response.data;
-            $scope.waiting = false;
-        }, function errorCallback(error){
-            console.log(error);
-            $scope.error = error;
-            $scope.waiting = false;
-        }
-    );
-
-    $scope.goTo = function(id){
-        NavBarService.pathStackPush('businesses', '/businesses/'+    $scope.businesssId , "#businesses/"+    $scope.businesssId )
-    }
-};
-
-
-angular
-    .module('clips.businesses')
-    .controller("BusinessesDetailsController", BusinessesDetailsController);
-
-BusinessesDetailsController.$inject = ['$scope', '$http', "NavBarService", "$routeParams"];
-=======
-/* recommended */
-var BusinessesDetailsController = function($scope, $http, NavBarService, $routeParams){
-    $scope.businesssId = $routeParams.businesssId
-    $scope.coupons = undefined;
-    $scope.business = undefined;
-    $scope.error = undefined;
-    $scope.waiting = true;
     $scope.center = {
 	lat: 42.0451,
 	lng: -87.6877,
@@ -75,7 +27,7 @@ var BusinessesDetailsController = function($scope, $http, NavBarService, $routeP
 	    }
 
 	    var html = $scope.business.name + '<br>' + $scope.business.address;
-	    
+
 	    $scope.markers = {
 		businessLoc: {
 		    lat: $scope.business.lat,
@@ -84,7 +36,7 @@ var BusinessesDetailsController = function($scope, $http, NavBarService, $routeP
 		    message: html
 		}
 	    }
-	    
+
             $scope.waiting = false;
         }, function errorCallback(error){
             console.log(error);
@@ -118,4 +70,3 @@ angular
     .controller("BusinessesDetailsController", BusinessesDetailsController);
 
 BusinessesDetailsController.$inject = ['$scope', '$http', "NavBarService", "$routeParams"];
->>>>>>> master
--- www/app/coupons/couponsDetail.controller.js
+++ www/app/coupons/couponsDetail.controller.js
@@ -2,84 +2,6 @@
  * Created by djdapz on 9/29/16.
  */
 /* recommended */
-var CouponsDetailController = function($scope, $routeParams, $http, UserCouponService){
-    $scope.couponId = $routeParams.couponId;
-    $scope.saving = false;
-    $scope.saved = false;
-
-    $scope.coupon = undefined;
-
-    $scope.error = undefined;
-    $scope.waiting = true;
-
-    $scope.saveCoupon = function(){
-      UserCouponService.save({userid: 1, couponid:   $scope.couponId}).$promise.then(
-        //success
-        function(value){
-          console.log('saved');
-          $scope.saved = true;
-        },
-
-        //error
-        function(error){
-          console.log('error');
-          $scope.error = error;
-        }
-      )
-    }
-
-    $scope.removeCoupon = function(){
-      UserCouponService.removeCoupon({userid: 1, couponid:  $scope.couponId}).$promise.then(
-        //success
-        function(value){
-          console.log('deleted');
-          $scope.saved = false;
-        },
-
-        //error
-        function(error){
-          console.log('error - delete');
-          $scope.error = error;
-        }
-      )
-    }
-
-
-    //add user clause and edit sql so we can know wether or not to show the save button
-    //TODO - make service
-    $http({
-        method: "GET",
-        url: " https://eecs394-clips-backend.herokuapp.com/coupon/" + $scope.couponId + "/user/" + 1
-    }).then(
-        function successCallback(response){
-            $scope.coupon = response.data[0];
-            if($scope.coupon.count == 0){
-              $scope.saved = false;
-            }else{
-              $scope.saved = true;
-            }
-            $scope.waiting = false;
-        }, function errorCallback(error){
-            console.log(error);
-            $scope.error = error;
-            $scope.waiting = false;
-        }
-    );
-
-
-};
-
-
-angular
-    .module('clips.coupons')
-    .controller("CouponsDetailController", CouponsDetailController);
-
-CouponsDetailController.$inject = ['$scope', "$routeParams", "$http", "UserCouponService"];
-=======
-/**
- * Created by djdapz on 9/29/16.
- */
-/* recommended */
 var CouponsDetailController = function($scope, $routeParams, $http){
     $scope.couponId = $routeParams.couponId
 
@@ -115,5 +37,3 @@ angular
     .controller("CouponsDetailController", CouponsDetailController);
 
 CouponsDetailController.$inject = ['$scope', "$routeParams", "$http"];
-
->>>>>>> master
